46472e38756fd5e6de3f9d894df81f527b058175
emadt10 <emadtariq1998@gmail.com>
2023-09-01 01:42:13 +0500

    first commit

---
+++ chpt1.js
@@ -0,0 +1,77 @@
+    //   CHAPTER NO 1  START       
+    //  Q-NO 1
+
+// var greetingMessage = "Hello Visitor";
+// alert(greetingMessage);
+
+// Q-2
+
+// var greetingMessage ="Error! Please enter a valid password";
+// alert(greetingMessage);
+
+//Q-3
+
+// var greetingMessage = "Welcome to js land...\n Happy Coding!";
+//  alert(greetingMessage);
+
+//Q-4
+
+// var greetingMessage = "Welcome to js land...";
+// alert(greetingMessage);
+                                         
+
+// var greetingMessage = "Happy Coding! \n ☐ Prevent this page from creatting additional diloug box";
+// alert(greetingMessage);
+
+//Q-5
+
+/*var greetingMessage = "Hello..i can run js through my web browser's console ";
+ alert(greetingMessage);*/
+
+
+                               
+                               
+// CHAPTER-2
+//Q1
+// var userName ;
+//Q2
+// var myName="Emad Tariq";
+
+//Q3
+// var message="HELLO! WORLD";
+// alert(message);
+
+//Q4
+// var namee=prompt("Enter your name");
+// var age=prompt("enter age");
+// var profession=prompt("enter Profession");
+// alert("Name = "+namee);
+// alert("Age = "+age);
+// alert("Occupation = "+profession);
+                               
+      
+
+//Q 5
+
+//   var greetingMessage ="PIZZA\nPIZZ\nPIZ\nPI\nP";
+//   alert(greetingMessage);
+
+//Q 6
+
+//   var email= prompt("Your email address");
+//   alert("your email is "+email);
+
+//Q 7
+
+// var book ="A Smarter Way to learn javascript";
+// alert("I am trying to learn from the book " +book);
+
+//Q 8
+// document.write("yah! I can write html content through java script");
+                                      
+
+//Q-9 
+var Mess ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(Mess);
+                                                                          
+                                    
